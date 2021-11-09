1e0e0134b105390a82d97ce967052a5ea6776840
Julia Tushchenko <juliayulianna@gmail.com>
2021-11-09 16:32:43 +0200

    1

--- src/index.js
+++ src/index.js
@@ -4,4 +4,13 @@ const DEBOUNCE_DELAY = 300;
 // const country = fetch(`https://restcountries.com/v3.1/name/${name}`);
 
 
-const country = fetch(`https://restcountries.com/v3.1/name/China`).then(data => {console.log(data);});
+const count = fetch(`https://restcountries.com/v3.1/name/China`)
+.then(response => {
+    return response.json();
+})
+.then((country) => {
+    console.log(country);
+})
+.catch((error) => {
+    console.log(error);
+});

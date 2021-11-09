8fa8791b77c192e72065e467653884a502a59507
Julia Tushchenko <juliayulianna@gmail.com>
2021-11-09 12:38:51 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+// const country = fetch(`https://restcountries.com/v3.1/name/${name}`);
+
+
+const country = fetch(`https://restcountries.com/v3.1/name/China`).then(data => {console.log(data);});

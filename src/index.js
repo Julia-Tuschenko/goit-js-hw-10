4947979ab9033e2eb0e2b5c9a4c7ff7e1dfeb36d
Julia Tushchenko <juliayulianna@gmail.com>
2021-11-10 11:38:46 +0200

    1

--- src/index.js
+++ src/index.js
@@ -3,14 +3,44 @@ import './css/styles.css';
 const DEBOUNCE_DELAY = 300;
 // const country = fetch(`https://restcountries.com/v3.1/name/${name}`);
 
+const refs = {
+    input: document.querySelector('#search-box'),
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+};
 
-const count = fetch(`https://restcountries.com/v3.1/name/China`)
-.then(response => {
-    return response.json();
-})
-.then((country) => {
-    console.log(country);
-})
-.catch((error) => {
-    console.log(error);
+console.log(refs.input);
+
+refs.input.addEventListener('input', (e) => {
+    e.preventDefault();
+    const name = refs.input.value;
+
+    fetchCountries(name)
+    .then((country) => {
+        console.log(country);
+    })
 });
+
+
+function fetchCountries(name){
+    return  fetch(`https://restcountries.com/v3.1/name/${name}`)
+    .then(response => {
+        response.json();
+    });
+}
+
+
+
+
+
+
+// const count = fetch(`https://restcountries.com/v3.1/name/China`)
+// .then(response => {
+//     return response.json();
+// })
+// .then((country) => {
+//     console.log(country);
+// })
+// .catch((error) => {
+//     console.log(error);
+// });
